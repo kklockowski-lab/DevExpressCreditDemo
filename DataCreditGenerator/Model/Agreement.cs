63501679e90284306e748388ce372d0e8eeb5482
kklockowski <k.j.klockowski@gmail.com>
2023-09-22 22:30:46 +0200

    Dodanie modelu Agreement.

---
+++ DataCreditGenerator/Model/Agreement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCreditGenerator.Model
+{
+    public class Agreement
+    {
+        public Client Client { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public uint DayOfPement { get; set; }
+
+
+        public double Amount { get; set; }
+        public uint Percent { get; set; }
+        public uint IinstallmentCount { get; set; }
+        public double Installment { get; set; }
+
+
+        public byte[] Attachment { get; set; }
+        public bool Active { get; set; }
+    }
+}
--- DataCreditGenerator/Model/Client.cs
+++ DataCreditGenerator/Model/Client.cs
@@ -14,7 +14,6 @@ namespace DataCreditGenerator.Model
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public bool Active { get; set; }
-
         public Gender Gender { get; set; }
     }
 }
